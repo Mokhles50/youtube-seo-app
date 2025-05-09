90fb0de615fc61d4d01a72fe1b61ffc396a34310
Mokhles50 <mokhlescharfeddine34@gmail.com>
2025-05-09 17:42:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -58,3 +58,5 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+    print("YouTube API Response:", response.status_code, response.text)
+
