4059bf72f6edea58f2aa8dd8b3cd9f6d0f6cbf3d
Mokhles50 <mokhlescharfeddine34@gmail.com>
2025-05-09 22:11:40 -0300

    app.py

--- app.py
+++ app.py
@@ -80,3 +80,7 @@ def index():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(debug=True, host="0.0.0.0", port=port)
+    @app.route("/blog/best-tool")
+def blog_best_tool():
+    return render_template("blog/best-tool.html")
+
